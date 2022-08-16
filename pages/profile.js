3757d23d4c58dc0312c81b236dd68206d893b6d7
Vineeth Voruganti <13438633+VVoruganti@users.noreply.github.com>
2022-08-15 23:24:12 -0700

    profile stuff

---
+++ pages/profile.js
@@ -0,0 +1,14 @@
+import Navbar from "../components/Navbar.jsx"
+import {
+    Title,
+} from "@mantine/core"
+
+export default function Home() {
+
+    return (
+        <div>
+            <Navbar />
+            <Title> In Progress </Title>
+        </div>
+    )
+}
